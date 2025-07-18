05b4eeb9756f49c093ccf1edd6b817163fc4afc6
Agoth24 <agoth.a3@gmail.com>
2025-07-17 22:48:17 -0600

    Dependencies linked, files are setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require("path");
-const HTMLWebpackPlugin = require(`html-webpack-plugin`)
+const HTMLWebpackPlugin = require(`html-webpack-plugin`);
 module.exports = {
   mode: `development`,
   entry: "./src/index.js",
   output: {
     filename: `main.js`,
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: `eval-source-map`,
   plugins: [
-new HTMLWebpackPlugin({
-    template: `./src/template.html`
-})
+    new HTMLWebpackPlugin({
+      template: `./src/template.html`,
+    }),
   ],
+  devServer: {
+    watchFiles: [`./src/template.html`],
+  },
   module: {
-    rules:[
-        {
-            test: '/\.css$/i',
-            use: ['style-loader', 'css-laoder'],
-        },
-        {
-            test: '/\.html$/i',
-            loader: `html-loader`,
-        },
-    ]
-  }
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: `./dist`,
+    hot: true,
+  },
 };
