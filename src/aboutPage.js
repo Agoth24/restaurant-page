f62b6bfd8af6f95033dbd37b975a9c86191c44d1
Agoth24 <agoth.a3@gmail.com>
2025-07-21 23:09:17 -0600

    Commit before I erase template.html's content

---
+++ src/aboutPage.js
@@ -0,0 +1,42 @@
+import "./about.css";
+
+const aboutTitle = document.createElement("h1");
+aboutTitle.classList.add("about-title");
+aboutTitle.textContent = "About Us";
+
+const aboutContainer = document.createElement("div");
+aboutContainer.classList.add("about-container");
+
+const aboutParas = [
+  "Welcome to BLOOM Café + Brunch, where coffee meets community and brunch is anything but basic.",
+  "We started with a simple idea: create a space where good people gather over great food. Whether you're here for your morning espresso, a slow Sunday brunch, or just a quiet corner with a book, our café was designed to feel like a second home.",
+  "Our kitchen focuses on seasonal ingredients, scratch-made dishes, and bold flavors. Think fresh sourdough, runny yolks, and locally roasted beans. From avocado toast to fried chicken waffles, everything is made with intention and care.",
+  "We believe in unrushed mornings, warm service, and thoughtful menus.",
+  "Thanks for making us part of your daily ritual."
+];
+
+const hoursTitle = document.createElement('h1')
+hoursTitle.classList.add('about-title')
+hoursTitle.textContent = 'Hours'
+
+const hoursContainer = document.createElement('div');
+hoursContainer.classList.add('hours-container');
+
+const hoursParas = [
+    "Sunday: 7:00AM - 7:00PM",
+    "Monday: 7:00AM - 7:00PM",
+    "Tuesday: 7:00AM - 7:00PM",
+    "Wednesday: 7:00AM - 7:00PM",
+    "Thursday: 7:00AM - 7:00PM",
+    "Friday: 7:00AM - 7:00PM",
+    "Saturday: 7:00AM - 7:00PM"
+]
+
+function addParasToDiv(paragraphArray, div) {
+paragraphArray.array.forEach(para => {
+    div.appendChild(para)
+});
+}
+
+addParasToDiv(aboutParas, aboutContainer)
+addParasToDiv(hoursParas, hoursContainer)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // Entry Point for project, writes to /dist/main.js
-import './global.css'
-import loadPage from "./loadpage";
-import homePageContent  from "./homePage";
-import menuPageContent from './menuPage';
+import "./global.css";
+import './about.css'
+// import loadPage from "./loadpage";
+// import homePageContent from "./homePage";
+// import menuPageContent from "./menuPage";
 
-loadPage(homePageContent(), 'home')
+// loadPage(homePageContent(), "home");
+
+// document.querySelector("header").addEventListener("click", (e) => {
+//   const target = e.target;
+    // if (target === header) return;
+    
+//   switch (e.target.id) {
+//     case ("nav-home" || "logo"):
+//       loadPage(homePageContent(), "home");
+//       break;
+//     case "nav-menu":
+//       loadPage(menuPageContent(), "menu");
+//       break;
+//   }
+// });
